5580ad8e2062630ae60ac0bb5b63f503c7251b43
solomon Abate <solomonabate18@gmail.com>
2024-06-03 16:05:58 +0300

    auth and user management init

---
+++ user-management-service/src/test/java/com/maraha/user_management/_service/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maraha.user_management._service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
